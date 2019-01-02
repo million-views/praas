37305d81cf360a17d8df7a10911eb2945866a397
Vakar Eeralil <varghese@m5nv.com>
2019-01-02 23:18:14 +0100

    Dummy test component to test route

--- praas-app/src/app/App.js
+++ praas-app/src/app/App.js
@@ -4,12 +4,19 @@ import { Router } from '@reach/router';
 const App = () => (
   <Router>
     <Home path="/" />
+    <Test path="/test" />
   </Router>
 );
 
 const Home = () => (
   <div>
     <h2> Welcome </h2>
+  </div>
+);
+
+const Test = () => (
+  <div>
+    <h2> Welcome test </h2>
   </div>
 );
 
