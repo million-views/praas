7e6f03a12cb096381f82d0fee56c1f3ba55d05dc
Vakar <varghese@m5nv.com>
2020-03-28 19:54:47 +0100

    signup with header nav

--- praas-app-ionic/src/pages/Signup.tsx
+++ praas-app-ionic/src/pages/Signup.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonButton, IonTitle } from '@ionic/react';
 class Signup extends React.Component {
   render() {
     return (
       <>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>App Name</IonTitle>
+            <IonTitle>PRAAS</IonTitle>
+            <IonButtons slot="end">
+              <IonButton fill="outline" slot="primary" href="/signup">
+                Signup
+              </IonButton>
+              <IonButton fill="outline" href="/login">
+                Login
+              </IonButton>
+            </IonButtons>
           </IonToolbar>
         </IonHeader>
       </>
