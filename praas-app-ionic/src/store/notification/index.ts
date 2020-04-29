39903741a68a1cecd2970c69a77ff41cbbf05654
Vakar <varghese@m5nv.com>
2020-04-29 13:13:41 +0200

    renaming and making notifications more customizable

--- praas-app-ionic/src/store/notification/index.ts
+++ praas-app-ionic/src/store/notification/index.ts
@@ -4,47 +4,55 @@ const NOTIFICATION_SUCCESS = 'notification/SUCCESS';
 const NOTIFICATION_WARNING = 'notification/WARNING';
 const NOTIFICATION_ERROR = 'notification/ERROR';
 
+type NotificationPayload = {
+  message: String;
+  duration?: Number;
+};
+
 // Sync action creators
 export const clearNotification = () => ({
   type: NOTIFICATION_CLEAR,
 });
 
-export const createSuccessNotification = (message) => {
+export const createSuccessNotification = (payload: NotificationPayload) => {
   return {
     type: NOTIFICATION_SUCCESS,
-    payload: message,
+    payload,
   };
 };
 
-export const createWarningNotification = (message) => ({
+export const createWarningNotification = (payload: NotificationPayload) => ({
   type: NOTIFICATION_WARNING,
-  payload: message,
+  payload,
 });
 
-export const createErrorNotification = (message) => ({
+export const createErrorNotification = (payload: NotificationPayload) => ({
   type: NOTIFICATION_ERROR,
-  payload: message,
+  payload,
 });
 
 // Reducer
-export default function notification(state = {}, { type, payload }) {
+export default function notification(
+  state = {},
+  { type, payload }: { type: string; payload: NotificationPayload }
+) {
   switch (type) {
     case NOTIFICATION_CLEAR:
       return {};
     case NOTIFICATION_SUCCESS:
       return {
         type: 'success',
-        message: payload,
+        data: payload,
       };
     case NOTIFICATION_WARNING:
       return {
         type: 'warning',
-        message: payload,
+        data: payload,
       };
     case NOTIFICATION_ERROR:
       return {
         type: 'danger',
-        message: payload,
+        data: payload,
       };
     default:
       return state;
