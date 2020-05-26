0048eb2e010dc0e80fb8ede717e8768e22de674c
Srinivas Gopalakrishnan <srinig@msn.com>
2020-05-26 13:20:03 +0530

    Removing hardcoded curi-length test
    - Curi length was being tested aganst a hardcoded value of 19
    - Curi prefix was also hardcoded in conduit api (makeCuri in POST)
    - Curi prefix was moved to system config
    - A new property called curiLength was added to systes config
    - The new property has a fixed value with comments on how it is calculated
    - Decided against a function for length as we could move config to a db tomorrow
    - The new property is now being used for the model test

--- crud-server/src/models/system.js
+++ crud-server/src/models/system.js
@@ -9,7 +9,14 @@ module.exports = {
     settings: {
       alphabet: '123456789abcdefghjkmnopqrstuvwxyz',
       uccount: 5,
+      prefix: 'td',
       domain: 'trickle.cc',
+      curiLen: 19,
+      // 2 + // this.prefix.length
+      // 1 + // for the '-' in makeCuri
+      // 5 + // this.uccount
+      // 1 + // for the '.' in makeCuri
+      // 10  // this.domain.length
     }
   }
 };
