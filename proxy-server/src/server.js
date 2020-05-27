66c82efc22887975afefaaaff3a43b8b19c6f333
Srinivas Gopalakrishnan <srinig@msn.com>
2020-05-27 22:06:38 +0530

    proxy-server,feat: auto-refresh of conduit cache
    - Fetching Conduits is now moved into a function by itself
    - After successful login, this function is called periodically.
    - The period is defined in config as cacheRefreshInterval (default 10 sec)
    - The function updates the proxy server conduits cache

--- proxy-server/src/server.js
+++ proxy-server/src/server.js
@@ -13,6 +13,10 @@ const app = express();
 
 app.use(cors());
 
+// store conduits indexed by curi in app.locals for lookup later...
+// Start with empty cache
+app.locals.cmap = new Map();
+
 // we handle all requests to the proxy end point...
 // TODO: this will evolve... soonish
 app.get('/', (req, res) => res.send('Hi there....PRaaS Proxy server is up and running!'));
@@ -73,44 +77,64 @@ function getProxyServerCredentials() {
   };
 }
 
+function padLeft(val, len=2, padChar='0') {
+  return (''+val).padStart(len, padChar);
+}
+
+function printTime() {
+  const now = new Date();
+  return now.getFullYear() +
+    '-' + padLeft((now.getMonth() + 1)) +
+    '-' + padLeft(now.getDate()) +
+    ' ' + padLeft(now.getHours()) +
+    ':' + padLeft(now.getMinutes()) +
+    ':' + padLeft(now.getSeconds());
+}
+
+async function fetchConduits (user) {
+  // console.log('logged in?', data);
+  // save our token...
+  global.localStorage.setItem('user', JSON.stringify({ ...user }));
+
+  // fetch a list of conduits... be sure to run test-model, test-rest in sequence
+  // before starting the proxy so we have data to test...
+  try {
+    const payload = await PraasAPI.conduit.list(user.id);
+    const conduits = payload.conduits;
+
+    // remove conduits which are not found in the list, from the cache
+    app.locals.cmap.forEach(cache => {
+      if (conduits.findIndex(list => list.curi === cache.curi) === -1) {
+        app.locals.cmap.delete(cache.curi);
+      }
+    });
+
+    // upsert conduits in cache from list received
+    for (let i=0, imax=conduits.length; i < imax; i++) {
+      const conduit = conduits[i];
+      app.locals.cmap.set(conduit.curi, conduit);
+    }
+
+    console.log(`${printTime()} : ${app.locals.cmap.size} active conduits`);
+  } catch (e) {
+    console.log('unexpected... ', e);
+  }
+};
+
 // launch the server and listen only when running as a standalone process
 if (!module.parent) {
   // fetch the conduits first... but first check if our credentials are kosher with resource server
   // by logging in...
-  PraasAPI.user.login(getProxyServerCredentials()).then(
-    async (data) => {
-      // console.log('logged in?', data);
-      // save our token...
-      localStorage.setItem('user', JSON.stringify({ ...data.user }));
+  PraasAPI.user.login(getProxyServerCredentials())
+    .then(async (data) => {
+      fetchConduits(data.user);
+      setInterval(() => fetchConduits(data.user), conf.cacheRefreshInterval);
+    })
+    .catch((error) => console.log('unexpected... ', error));
 
-      // fetch a list of conduits... be sure to run test-model, test-rest in sequence
-      // before starting the proxy so we have data to test...
-      try {
-        const payload = await PraasAPI.conduit.list(data.user.id);
-        const conduits = payload.conduits;
-        let conduit;
-        // console.log('conduits... yay!', typeof(conduits), conduits);
-
-        // store conduits indexed by curi in app.locals for lookup later...
-        const cmap = new Map();
-        for (let i=0, imax=conduits.length; i < imax; i++) {
-          conduit = conduits[i];
-          cmap.set(conduit.curi, conduit);
-        }
-        app.locals.cmap = cmap;
-        console.log(`${app.locals.cmap.size} active conduits received`);
-      } catch (e) {
-        console.log('unexpected... ', e);
-      }
-
-      // start listening only after logging in to the resource server...
-      // if we can't login then there's no point in running the proxy
-      app.listen(conf.port, () => {
-        console.log(`Conduits proxy server is listening on port ${conf.port}`);
-      });
-    },
-    (error) => {
-      console.log('unexpected... ', error);
-    }
-  );
+  // start listening only after logging in to the resource server...
+  // if we can't login then there's no point in running the proxy
+  app.listen(conf.port, () => {
+    console.log(`Conduits proxy server is listening on port ${conf.port}`);
+  });
 }
