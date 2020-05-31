8a6d1a27c53f3873f6a4511fde573cd5f208b799
Vakar <varghese@m5nv.com>
2020-05-31 15:30:48 +0200

    better styling for empty state

--- praas-app-ionic/src/pages/HomePage/index.tsx
+++ praas-app-ionic/src/pages/HomePage/index.tsx
@@ -10,6 +10,7 @@ import {
   IonIcon,
   IonFab,
   IonFabButton,
+  IonText,
 } from '@ionic/react';
 import { add, createOutline, trashOutline } from 'ionicons/icons';
 import { connect } from 'react-redux';
@@ -28,58 +29,66 @@ const Home: React.FC<Props> = ({ conduits, listConduits }) => {
     listConduits();
   }, []);
   return (
-    <IonPage className="home-page">
+    <IonPage>
       <Header />
-      <IonContent>
-        <IonGrid className="table">
-          <IonRow className="table-header">
-            <IonCol className="table-header-col">Description</IonCol>
-            <IonCol className="table-header-col">Type</IonCol>
-            <IonCol className="table-header-col">Service Endpoint</IonCol>
-            <IonCol className="table-header-col">Status</IonCol>
-            <IonCol className="table-header-col">Action</IonCol>
+      <IonContent className="home-page">
+        {!conduits.length && (
+          <IonRow className="ion-justify-content-center no-conduits">
+            <IonCol className="ion-text-center">
+              <IonText>
+                <h3>
+                  You have no conduits available. Click on the + button to
+                  create one
+                </h3>
+              </IonText>
+            </IonCol>
           </IonRow>
-          {!conduits.length && (
-            <IonRow key="empty-table" className="table-row">
-              <IonCol className="table-col ion-text-center">
-                No conduits available.
-              </IonCol>
+        )}
+        {!!conduits.length && (
+          <IonGrid fixed className="table">
+            <IonRow className="table-header">
+              <IonCol className="table-header-col">Description</IonCol>
+              <IonCol className="table-header-col">Type</IonCol>
+              <IonCol className="table-header-col">Service Endpoint</IonCol>
+              <IonCol className="table-header-col">Status</IonCol>
+              <IonCol className="table-header-col">Action</IonCol>
             </IonRow>
-          )}
-          {conduits.map((conduit) => {
-            return (
-              <IonRow key={conduit.id} className="table-row">
-                <IonCol className="table-col">{conduit.description}</IonCol>
-                <IonCol className="table-col">{conduit.suriType}</IonCol>
-                <IonCol className="table-col">{conduit.suri}</IonCol>
-                <IonCol className="table-col">{conduit.status}</IonCol>
-                <IonCol className="table-col">
-                  <IonButtons>
-                    <IonButton
-                      type="button"
-                      fill="clear"
-                      size="small"
-                      href={`conduit/${conduit.id}  `}
-                    >
-                      <IonIcon className="icon edit" icon={createOutline} />
-                      <i className="far fa-edit"></i>
-                    </IonButton>
-                    <IonButton
-                      type="button"
-                      fill="clear"
-                      size="small"
-                      onClick={noop}
-                    >
-                      <IonIcon className="icon delete" icon={trashOutline} />
-                    </IonButton>
-                  </IonButtons>
-                </IonCol>
-              </IonRow>
-            );
-          })}
-        </IonGrid>
+
+            {conduits.map((conduit) => {
+              return (
+                <IonRow key={conduit.id} className="table-row">
+                  <IonCol className="table-col">{conduit.description}</IonCol>
+                  <IonCol className="table-col">{conduit.suriType}</IonCol>
+                  <IonCol className="table-col">{conduit.suri}</IonCol>
+                  <IonCol className="table-col">{conduit.status}</IonCol>
+                  <IonCol className="table-col">
+                    <IonButtons>
+                      <IonButton
+                        type="button"
+                        fill="clear"
+                        size="small"
+                        href={`conduit/${conduit.id}`}
+                      >
+                        <IonIcon className="icon edit" icon={createOutline} />
+                        <i className="far fa-edit"></i>
+                      </IonButton>
+                      <IonButton
+                        type="button"
+                        fill="clear"
+                        size="small"
+                        onClick={noop}
+                      >
+                        <IonIcon className="icon delete" icon={trashOutline} />
+                      </IonButton>
+                    </IonButtons>
+                  </IonCol>
+                </IonRow>
+              );
+            })}
+          </IonGrid>
+        )}
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton href="/conduit/create">
+          <IonFabButton color="primary" href="/conduit/create">
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
