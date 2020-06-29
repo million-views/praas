5b7f06c289f51abd2bc6bb43f86d7761be034220
Srinivas Gopalakrishnan <srinig@msn.com>
2020-06-29 14:27:47 +0530

    crud-server,feature: Create Replace Conduit API
    - Added PUT handler to replace existing conduit
    - Made code more readable by limiting lines to 80 characters
    - Hoisted conduitReqdFIelds and conduitOptFields to top of file as they are reused
    - Removed unnecessay setting of default values in POST handler
    
    Issue #97

--- crud-server/src/routes/api/conduits.js
+++ crud-server/src/routes/api/conduits.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
-const models = require('../../models');
-const Conduit = require('../../models').Conduit;
+const { Op } = require('sequelize');
+
 const auth = require('../auth');
 const helpers = require('../../lib/helpers');
-const Op = require('sequelize').Op;
+const { System } = require('../../models');
+const { Conduit } = require('../../models');
+
+const conduitReqdFields = [
+  'suriApiKey',
+  'suriObjectKey',
+  'suriType',
+  'suri',
+];
+
+const conduitOptFields = [
+  'throttle', // default: true
+  'status', // default: inactive
+  'description', // nulls allowed
+  'hiddenFormField', // default: []
+  'whitelist', // default: []
+  'racm', // default: []
+];
 
 // add conduit
 router.post('/', auth.required, async function (req, res, next) {
@@ -11,94 +28,141 @@ router.post('/', auth.required, async function (req, res, next) {
   const errors = {};
 
   conduit.userId = req.payload.id;
-  conduit.curi = await helpers.makeCuri(models.System.cconf.settings.prefix);
-  conduit.throttle = true;
-  conduit.status = 'Inactive';
+  conduit.curi = await helpers.makeCuri(System.cconf.settings.prefix);
 
-  const conduitReqdFields = ['suriApiKey', 'suriObjectKey', 'suriType', 'suri', 'whitelist', 'racm'];
-  const conduitOptFields = ['throttle', 'status', 'description', 'hiddenFormField'];
-  helpers.processInput(await req.body.conduit, conduitReqdFields, conduitOptFields, conduit, errors);
+  helpers.processInput(
+    await req.body.conduit,
+    conduitReqdFields,
+    conduitOptFields,
+    conduit,
+    errors
+  );
+
   if (Object.keys(errors).length) return res.status(422).json({ errors });
 
   try { await conduit.save(); } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       return res.status(422).json({ error });
     }
+    // In case the generated curi is a duplicate, we try once more
     if (error.name === 'SequelizeUniqueConstraintError') {
-      conduit.curi = await helpers.makeCuri(models.System.cconf.settings.prefix);
+      conduit.curi = await helpers.makeCuri(System.cconf.settings.prefix);
       await conduit.save();
     } else return next(error);
   };
 
-  return res.status(201).json({ conduit: { id: conduit.id } });
+  return res.status(201).json({
+    conduit: {
+      id: conduit.id,
+      curi: conduit.curi
+    }
+  });
 });
 
 // Get conduit
 router.get('/:id', auth.required, async (req, res, next) => {
   try {
-    const conduit = await Conduit.findOne({ where: { id: req.params.id, userId: req.payload.id } });
-    if (!conduit) { return res.sendStatus(404); }
+    const conduit = await Conduit.findOne({
+      where: {
+        id: req.params.id,
+        userId: req.payload.id
+      }
+    });
+    if (!conduit) return res.sendStatus(404);
 
     return res.json({ conduit: conduit.toJSON() });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
 });
 
 // Get all conduits + batch (start & count)
 // TODO: add ACL/scope check when integrated with OAuth2
-// For now we are hacking to support proxy-server functionality without adding complexity
+// For now we are hacking to support proxy-server functionality
+// without adding complexity
 router.get('/', auth.required, async (req, res, next) => {
   try {
     let conduits = undefined;
-    if (typeof req.query.start !== 'undefined' && typeof req.query.count !== 'undefined') {
+    if (typeof req.query.start !== 'undefined' &&
+        typeof req.query.count !== 'undefined'
+    ) {
       conduits = await Conduit.findAll({
-        where: { id: { [Op.gte]: req.query.start, [Op.lt]: +req.query.start+ +req.query.count }, userId: req.payload.id }
+        where: {
+          id: {
+            [Op.gte]: req.query.start,
+            // sgk: Prefix + to convert from string to number
+            [Op.lt]: +req.query.start + +req.query.count,
+          },
+          userId: req.payload.id,
+        }
       });
     } else {
-      if (req.app.locals.proxyUser && req.app.locals.proxyUser.id === req.payload.id) {
-        // fetch all conduits in active status... TODO: this is brittle and can break
+      if (req.app.locals.proxyUser &&
+          req.app.locals.proxyUser.id === req.payload.id
+      ) {
+        // fetch all conduits in active status...
+        // TODO: this is brittle and can break
         conduits = await Conduit.findAll({ where: { status: 'active' } });
       } else {
         conduits = await Conduit.findAll({ where: { userId: req.payload.id } });
       }
     }
 
-    if (!conduits) { return res.sendStatus(404); }
+    if (!conduits) return res.sendStatus(404);
+
     return res.json({ conduits: conduits.map(i => i.toJSON()) });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) { next(error); }
+});
+
+// Replace conduit
+router.put('/:id', auth.required, async (req, res, next) => {
+  try {
+    const conduit = await Conduit.findByPk(req.params.id);
+    if (!conduit) return res.sendStatus(404);
+    if (req.body.conduit.curi) return res.sendStatus(400);
+
+    const errors = {};
+    const newCdt = new Conduit();
+    const objCdt = newCdt.toJSON();
+    delete objCdt.id;
+    delete objCdt.curi;
+    delete objCdt.userId;
+    delete conduit.description;
+    Object.assign(conduit, objCdt);
+
+    helpers.processInput(
+      req.body.conduit,
+      conduitReqdFields,
+      conduitOptFields,
+      conduit,
+      errors
+    );
+
+    if (Object.keys(errors).length) return res.status(422).json({ errors });
+
+    await conduit.update(req.body.conduit);
+    res.status(200).json({ conduit: conduit.toJSON() });
+  } catch (error) { return next(error); }
 });
 
 // Update conduit
 router.patch('/:id', auth.required, async (req, res, next) => {
   try {
     const conduit = await Conduit.findByPk(req.params.id);
-    if (!conduit) {
-      return res.sendStatus(404);
-    }
-    if (req.body.conduit.curi) {
-      return res.sendStatus(400);
-    }
+    if (!conduit) return res.sendStatus(404);
+    if (req.body.conduit.curi) return res.sendStatus(400);
 
     await conduit.update(await req.body.conduit);
-    res.status(200).json({ conduit: conduit.toJSON() });
-  } catch (error) {
-    return next(error);
-  }
+    return res.status(200).json({ conduit: conduit.toJSON() });
+  } catch (error) { return next(error); }
 });
 
 // Delete conduit
 router.delete('/:id', auth.required, async (req, res, next) => {
   try {
-    // const count = await Conduit.destroy({ where: { id: req.params.id, userId: req.payload.id } });
     const count = await Conduit.destroy({ where: { id: req.params.id } });
-    // return count ? res.sendStatus(200) : res.sendStatus(404);
-    return count ? res.status(200).json({ conduit: { id: req.params.id } }) : res.sendStatus(404);
-  } catch (error) {
-    return next(error);
-  }
+    return count ?
+      res.status(200).json({ conduit: { id: req.params.id } }) :
+      res.sendStatus(404);
+  } catch (error) { return next(error); }
 });
 
 module.exports = router;
