c7babec4aa867e822e9bbf6f6c6957ff4c695a5a
Kavitha Pugal <kavitha@m5nv.com>
2020-07-09 18:22:37 +0530

    prass-app-ionic, wip:#134 added the delete button to UI

--- praas-app-ionic/src/components/Conduit/Form/IPAllowList/AllowlistItem.tsx
+++ praas-app-ionic/src/components/Conduit/Form/IPAllowList/AllowlistItem.tsx
@@ -1,40 +1,48 @@
 import React from 'react';
-import { IonRow, IonCol, IonLabel, IonGrid } from '@ionic/react';
+import { IonRow, IonCol, IonLabel, IonGrid, IonButton } from '@ionic/react';
 import { FieldError } from 'react-hook-form';
 import Input from '../../../../components/Form/Input';
 import RadioGroup from '../../../../components/Form/RadioGroup';
 import FormFieldWithError from '../../../../components/FormFieldWithError';
 
 type Props = {
+  index: number;
   item: any;
   prefix: string;
   error: { ip?: FieldError; comment?: FieldError; status?: FieldError };
   onDelete: (idx: number) => void;
 };
-const AllowListItem = ({ item, prefix, onDelete, error }: Props) => {
+const AllowListItem = ({ index, item, prefix, onDelete, error }: Props) => {
   return (
     <IonGrid className="ip-allowlist">
       <IonRow>
-        <FormFieldWithError error={error?.ip}>
-          <IonLabel position="floating">IP Address</IonLabel>
-          <Input name={`${prefix}.ip`} value={item.ip} />
-        </FormFieldWithError>
+        <IonCol>
+          <FormFieldWithError error={error?.ip}>
+            <IonLabel position="floating">IP Address</IonLabel>
+            <Input name={`${prefix}.ip`} value={item.ip} />
+          </FormFieldWithError>
 
-        <FormFieldWithError error={error?.comment}>
-          <IonLabel position="floating">Comment</IonLabel>
-          <Input name={`${prefix}.comment`} value={item.comment} />
-        </FormFieldWithError>
+          <FormFieldWithError error={error?.comment}>
+            <IonLabel position="floating">Comment</IonLabel>
+            <Input name={`${prefix}.comment`} value={item.comment} />
+          </FormFieldWithError>
+        </IonCol>
+        <IonCol>
+          <IonButton type="button" onClick={() => onDelete(index)}>
+            X
+          </IonButton>
+        </IonCol>
       </IonRow>
       <IonRow className="ip-allowlist-status">
-        <RadioGroup
-          labelClassName="ip-allowlist-status__label"
-          name={`${prefix}.status`}
-          value={item.status}
-          options={[
-            { value: 'active', label: 'Active' },
-            { value: 'inactive', label: 'Inactive' },
-          ]}
-        />
+          <RadioGroup
+            labelClassName="ip-allowlist-status__label"
+            name={`${prefix}.status`}
+            value={item.status}
+            options={[
+              { value: 'active', label: 'Active' },
+              { value: 'inactive', label: 'Inactive' },
+            ]}
+          />
       </IonRow>
     </IonGrid>
   );
