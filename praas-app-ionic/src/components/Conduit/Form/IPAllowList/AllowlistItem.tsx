d826523122ed2aab61942424d90e82717c9f5132
Vakar <varghese@m5nv.com>
2020-07-19 10:08:45 +0200

    ip details header added

--- praas-app-ionic/src/components/Conduit/Form/IPAllowList/AllowlistItem.tsx
+++ praas-app-ionic/src/components/Conduit/Form/IPAllowList/AllowlistItem.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { IonRow, IonCol, IonLabel, IonGrid, IonButton } from '@ionic/react';
+import {
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonGrid,
+  IonButton,
+  IonText,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardSubtitle,
+} from '@ionic/react';
 import { FieldError } from 'react-hook-form';
 import Input from '../../../../components/Form/Input';
 import RadioGroup from '../../../../components/Form/RadioGroup';
@@ -14,19 +25,26 @@ type Props = {
 };
 const AllowListItem = ({ index, item, prefix, error }: Props) => {
   return (
-    <IonGrid className="ip-allowlist">
-      <IonRow>
-          <FormFieldWithError error={error?.ip}>
-            <IonLabel position="floating">IP Address</IonLabel>
-            <Input name={`${prefix}.ip`} value={item.ip} />
-          </FormFieldWithError>
-
-          <FormFieldWithError error={error?.comment}>
-            <IonLabel position="floating">Comment</IonLabel>
-            <Input name={`${prefix}.comment`} value={item.comment} />
-          </FormFieldWithError>
-      </IonRow>
-      <IonRow className="ip-allowlist-status">
+    <IonCard className="ion-no-margin ion-margin-bottom">
+      <IonCardHeader>
+        <IonCardSubtitle>IP Details</IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <IonRow className="ip-allowlist">
+          <IonCol size="12" sizeMd="6">
+            <FormFieldWithError error={error?.ip}>
+              <IonLabel position="floating">IP Address</IonLabel>
+              <Input name={`${prefix}.ip`} value={item.ip} />
+            </FormFieldWithError>
+          </IonCol>
+          <IonCol size="12" sizeMd="6">
+            <FormFieldWithError error={error?.comment}>
+              <IonLabel position="floating">Comment</IonLabel>
+              <Input name={`${prefix}.comment`} value={item.comment} />
+            </FormFieldWithError>
+          </IonCol>
+        </IonRow>
+        <IonRow className="ip-allowlist-status">
           <RadioGroup
             labelClassName="ip-allowlist-status__label"
             name={`${prefix}.status`}
@@ -36,8 +54,9 @@ const AllowListItem = ({ index, item, prefix, error }: Props) => {
               { value: 'inactive', label: 'Inactive' },
             ]}
           />
-      </IonRow>
-    </IonGrid>
+        </IonRow>
+      </IonCardContent>
+    </IonCard>
   );
 };
 
