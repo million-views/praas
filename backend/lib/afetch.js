9d19bfd20097901993e35a760c7b2eae681691e8
M5NV Developer <developer@m5nv.com>
2020-09-01 02:54:38 +0530

    backend/lib: move afetch out of praas for reuse in access-token

---
+++ backend/lib/afetch.js
@@ -0,0 +1,81 @@
+// afetch => application specific fetch wrapper
+//
+// Most of the time we need a way to be able to distinguish between a network
+// and programmatic errors versus a response from server that indicates an
+// erroneous request.
+//
+// The distinction between these two conditions leaks all over the place in
+// in an application when using fetch directly.  This is a tiny wrapper
+// around `fetch` that allows an application to centralize such code in one
+// place.
+
+const fetch = require('node-fetch');
+
+// throws type error if parameters is not iterable and that is by design...
+// don't call this function when there are no query parameters.
+const queryize = (parameters) => {
+  return Object.entries(parameters).reduce((acc, entry, index) => {
+    const [param, value] = entry;
+    const encoded =
+      index === 0
+        ? `${param}=${encodeURIComponent(value)}&`
+        : `${param}=${encodeURIComponent(value)}`;
+    return `${acc}${encoded}`;
+  }, '');
+};
+
+// applies base service endpoint and optionally creates url query
+// string when params is an object.
+const urlize = (host, path, params = undefined) => {
+  let url = `${host}${path}`;
+  if (params) {
+    url = `${url}?${queryize(params)}`;
+  }
+
+  return url;
+};
+
+// tiny application specific wrapper around fetch. In its current form
+// it assumes all responses including errors to be in JSON format.
+// as the need arises.
+// @host hostname including port number if any (e.g http://localhost:4000)
+// @path path to RESTful resource (e.g /conduits)
+// @options {headers, parameters, onError, ...rest}
+//
+// onError in options is invoked for non-2xx responses; all other errors
+// `reject`ed.
+async function afetch(host, path, options) {
+  const { headers, parameters, onError, ...rest } = options;
+  try {
+    const response = await fetch(urlize(host, path, parameters), {
+      ...rest,
+      headers,
+    });
+    // this handles server response (including non 2xx)
+    if (response.ok) {
+      return response.json();
+    } else {
+      if (onError) {
+        return onError(response);
+      } else {
+        // caller wants server errors to be treated as error
+        // eslint-disable-next-line prefer-promise-reject-errors
+        return Promise.reject({ ...response });
+      }
+    }
+  } catch (error) {
+    console.log('Got into an error situation');
+    // this path is for network or internal programming and networl errors
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return Promise.reject({
+      statusText: "I'm a teapot",
+      status: 418, // Unable to contact server, can't make coffee,
+      errors: {
+        offline: 'Check host name and confirm server is up and accessible',
+        network: error.message,
+      },
+    });
+  }
+}
+
+module.exports = afetch;
--- backend/lib/praas.js
+++ backend/lib/praas.js
@@ -1,33 +1,10 @@
-const fetch = require('node-fetch');
+const afetch = require('./afetch');
 
 // localStorage-polyfill is meant for development but we are
 // abusing it here to get the proxy server functional...
 require('localstorage-polyfill');
 
-const API_URL = 'http://localhost:4000';
-
-// throws type error if parameters is not iterable and that is by design...
-// don't call this function when there are no query parameters.
-const queryize = (parameters) => {
-  return Object.entries(parameters).reduce((acc, entry, index) => {
-    const [param, value] = entry;
-    const encoded =
-      index === 0
-        ? `${param}=${encodeURIComponent(value)}&`
-        : `${param}=${encodeURIComponent(value)}`;
-    return `${acc}${encoded}`;
-  }, '');
-};
-
-// applies base service endpoint and optionally creates url query
-// string when params is an object.
-const urlize = (service, params = undefined) => {
-  service = `${API_URL}${service}`;
-  if (params) {
-    service = `${service}?${queryize(params)}`;
-  }
-  return service;
-};
+const API_HOST = 'http://localhost:4000';
 
 const authorization = () => {
   // we store jwt token in localStorage; there are healthy debates
@@ -50,117 +27,101 @@ const invalidateSession = () => {
   global.localStorage.removeItem('user');
 };
 
-// This is a custom fetch that is coded to work with the specification
-// of the backend REST api server. In simple terms it expects all responses
-// including errors to be in JSON format. Exception for image loading for
-// example, will be accomodated within this function as the need arises.
-//
-const afetch = async (url, { headers, parameters, ...rest }) => {
-  headers = {
-    ...headers,
+function headers() {
+  return {
     ...authorization(),
     Accept: 'application/json',
     'Content-Type': 'application/json',
   };
+}
 
-  return fetch(urlize(url, parameters), {
-    ...rest,
-    headers,
-  }).then(
-    async (response) => {
-      // this handles server response (including non 2xx)
-      // we can dispatch both success and non-success action creators
-      // from here...
-      if (response.ok) {
-        return response.json();
-      } else {
-        const errors = await response.json();
-        console.log('error: ', errors);
-        if (response.status === 401) {
-          // token expired? clear our view of logged in status
-          invalidateSession();
-          // reload current page to kickstart a new session
-          // location.reload(true);
-        }
+// This handles server response with a status code other than 2xx...
+// ... which is distinct from network and other errors when using fetch
+async function onServerError(response) {
+  const errors = await response.json();
+  console.log('error: ', errors);
+  if (response.status === 401) {
+    // token expired? clear our view of logged in status
+    invalidateSession();
+  }
 
-        // eslint-disable-next-line prefer-promise-reject-errors
-        return Promise.reject({
-          statusText: response.statusText,
-          status: response.status,
-          ...errors,
+  // eslint-disable-next-line prefer-promise-reject-errors
+  return Promise.reject({
+    statusText: response.statusText,
+    status: response.status,
+    ...errors,
+  });
+}
+
+function praas() {
+  return {
+    user: {
+      register(data) {
+        return afetch(API_HOST, '/users', {
+          method: 'POST',
+          headers: headers(),
+          body: JSON.stringify(data),
+          // parameters: { start: 10, count: 20 }
+          onError: onServerError,
         });
-      }
+      },
+      login(data) {
+        return afetch(API_HOST, '/users/login', {
+          method: 'POST',
+          headers: headers(),
+          body: JSON.stringify(data),
+          onError: onServerError,
+        });
+      },
+      logout() {
+        return Promise.resolve('success').then(() => invalidateSession());
+      },
     },
-    (error) => {
-      console.log('Got into an error situation');
-      // this path is for network or internal programming errors
-      // eslint-disable-next-line prefer-promise-reject-errors
-      return Promise.reject({
-        statusText: "I'm a teapot",
-        status: 418, // Unable to contact server, can't make coffee,
-        errors: {
-          offline: 'Check to see Conduits resource server is up and running',
-          network: error.message,
-        },
-      });
-    }
-  );
-};
-
-const praas = {
-  user: {
-    register(data) {
-      return afetch('/users', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        // parameters: { start: 10, count: 20 }
-      });
+    conduit: {
+      add(data) {
+        return afetch(API_HOST, '/conduits', {
+          method: 'POST',
+          headers: headers(),
+          body: JSON.stringify(data),
+          onError: onServerError,
+        });
+      },
+      update(data) {
+        const cid = data.conduit.id;
+        return afetch(API_HOST, `/conduits/${cid}`, {
+          method: 'PATCH',
+          headers: headers(),
+          body: JSON.stringify(data),
+          onError: onServerError,
+        });
+      },
+      get(id) {
+        return afetch(API_HOST, `/conduits/${id}`, {
+          method: 'GET',
+          headers: headers(),
+          onError: onServerError,
+        });
+      },
+      list(_id) {
+        // id is not used
+        return afetch(API_HOST, '/conduits', {
+          method: 'GET',
+          headers: headers(),
+          onError: onServerError,
+        });
+      },
+      delete(data) {
+        console.log('data: ', data);
+        const cid = data;
+        return afetch(API_HOST, `/conduits/${cid}`, {
+          method: 'DELETE',
+          headers: headers(),
+          onError: onServerError,
+        });
+      },
     },
-    login(data) {
-      return afetch('/users/login', {
-        method: 'POST',
-        body: JSON.stringify(data),
-      });
-    },
-    logout() {
-      return Promise.resolve('success').then((_success) => invalidateSession());
-    },
-  },
-  conduit: {
-    add(data) {
-      return afetch('/conduits', {
-        method: 'POST',
-        body: JSON.stringify(data),
-      });
-    },
-    update(data) {
-      const cid = data.conduit.id;
-      return afetch(`/conduits/${cid}`, {
-        method: 'PATCH',
-        body: JSON.stringify(data),
-      });
-    },
-    get(id) {
-      return afetch(`/conduits/${id}`, {
-        method: 'GET',
-      });
-    },
-    list(_id) {
-      // id is not used
-      return afetch('/conduits', {
-        method: 'GET',
-        // body: JSON.stringify(id),
-      });
-    },
-    delete(data) {
-      console.log('data: ', data);
-      const cid = data;
-      return afetch(`/conduits/${cid}`, {
-        method: 'DELETE',
-      });
-    },
-  },
-};
+  };
+}
 
 // export default praas;
-module.exports = praas;
+module.exports = praas();
