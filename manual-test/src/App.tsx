8de11fdd6b4a916abaa6c2bef8de2f7cc80fec4a
Vakar <varghese@m5nv.com>
2020-09-13 08:15:48 +0200

    initial layout for wireframe

--- manual-test/src/App.tsx
+++ manual-test/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Provider, defaultTheme, Button } from '@adobe/react-spectrum';
+import { Provider, defaultTheme, Grid, View } from '@adobe/react-spectrum';
 
 function App() {
   return (
     <Provider theme={defaultTheme}>
-      <Button variant="cta" onPress={() => alert('Hey there!')}>
-        Hello React Spectrum!
-      </Button>
+      <Grid
+        areas={[
+          'submit  submit-response',
+          'evs evs-response',
+          'ravs  ravs-response',
+        ]}
+        columns={['3fr', '3fr']}
+        rows={['auto', 'auto', 'auto']}
+        height="size-6000"
+        gap="size-100"
+      >
+        <View backgroundColor="celery-600" gridArea="submit" />
+        <View backgroundColor="celery-600" gridArea="submit-response" />
+        <View backgroundColor="blue-600" gridArea="evs" />
+        <View backgroundColor="purple-600" gridArea="evs-response" />
+        <View backgroundColor="magenta-600" gridArea="ravs" />
+        <View backgroundColor="magenta-600" gridArea="ravs-response" />
+      </Grid>
     </Provider>
   );
 }
