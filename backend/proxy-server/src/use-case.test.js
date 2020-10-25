312a05f788ae5a8e8feb5faab1843ba165819757
Kavitha Pugal <kavitha@m5nv.com>
2020-10-25 19:01:18 +0530

    proxy-server: added util functions to validate email address
    add test case to generate status report from the spreadsheet

--- backend/proxy-server/src/use-case.test.js
+++ backend/proxy-server/src/use-case.test.js
@@ -4,6 +4,8 @@ const path = require('path');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const util = require('../../lib/util');
+
 const expect = chai.expect;
 chai.use(chaiHttp);
 
@@ -20,6 +22,13 @@ const testConduits = JSON.parse(
 const conduitA = testConduits.conduitA;
 const conduitB = testConduits.conduitB;
 const conduitC = testConduits.conduitC;
+
+let realSubmissioncount, botSubmissionCount, validCount, invalidCount;
+
+const stats = {
+  submissions: { realSubmissioncount, botSubmissionCount },
+  processes: { validCount, invalidCount },
+};
 
 const request1 = {
   records: [
@@ -46,7 +55,7 @@ const request2 = {
     {
       fields: {
         name: 'Jack L2uc',
-        email: 'jack.2uc@last.com',
+        email: 'jack$$$2uc@last.com',
         hiddenFormField: 'hff-5',
       },
     },
@@ -60,7 +69,7 @@ const request2 = {
     {
       fields: {
         name: 'Jack L4uc',
-        email: 'jack.4uc@last.com',
+        email: 'jack.4uclast.com',
         hiddenFormField: 'hff-5',
       },
     },
@@ -81,8 +90,64 @@ const request2 = {
     {
       fields: {
         name: 'Jack L7uc',
-        email: 'jack.7uc@last.com',
+        email: 'jack.7uc@last',
         hiddenFormField: 'hff-5',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L1uc',
+        email: 'jack.1uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L2uc',
+        email: 'jack.2uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L3uc',
+        email: 'jack.3uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L4uc',
+        email: 'jack.4uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L5uc',
+        email: 'jack.5uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L6uc',
+        email: 'jack.6uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L7uc',
+        email: 'jack.7uc@last.com',
+        hiddenFormField: 'hff',
+      },
+    },
+    {
+      fields: {
+        name: 'Jack L8uc',
+        email: 'jack.8uc@last.com',
+        hiddenFormField: 'hff',
       },
     },
   ],
@@ -152,7 +217,7 @@ describe('Registered user creates three conduits to a previously setup `contacts
   context(
     'Registered user `hands-over` `conduit-A` to a widget-designer',
     () => {
-      it('should have write permission to create a form', function () {
+      xit('should have write permission to create a form', function () {
         // console.log('>> conduit A : ', conduitA.racm);
         expect(conduitA.racm).to.eql(['POST']);
       });
@@ -161,21 +226,32 @@ describe('Registered user creates three conduits to a previously setup `contacts
   context(
     ' Registered user `publishes` a page with the embedded form ',
     () => {
-      it('Visitors provide their `first-name` and `email` | N submissions', async function () {
+      xit('Visitors provide their `first-name` and `email` | N submissions', async function () {
         const res = await proxyServer()
           .post('/')
           .set('Host', conduitA.host)
           .send(request2);
-        console.debug(res.body, res.status, res.error);
+        console.log('>>> request2 length:', request2.records.length);
+        // console.debug(res.body, res.status, res.error);
+        console.log('>>> response length:', res.body.records.length);
         expect(res.status).to.equal(200);
+        stats.submissions.realSubmissioncount = res.body.records.length;
+        stats.submissions.botSubmissionCount =
+          request2.records.length - res.body.records.length;
       });
-      it('Bots have a field day and spam the heck out on the gateway | M submissions', async function () {
+      xit('Bots have a field day and spam the heck out on the gateway | M submissions', async function () {
         const res = await proxyServer()
           .post('/')
           .set('Host', conduitA.host)
           .send(request3);
-        console.debug(res.body, res.status, res.error);
+        // console.debug(res.body, res.status, res.error);
         expect(res.status).to.equal(200);
+        if (res.body.records === undefined) {
+          stats.submissions.botSubmissionCount = request3.records.length;
+        } else {
+          stats.submissions.botSubmissionCount =
+            request3.records.length - res.body.records.length;
+        }
       });
     }
   );
@@ -183,13 +259,49 @@ describe('Registered user creates three conduits to a previously setup `contacts
     'Registered user `hands-over` `conduit-B` to a 3rd party email validation service (EVS)',
     () => {
       it('should have get and patch permission', async function () {
-        expect(conduitB.racm).to.eql(['GET', 'PATCH']);
+        expect(conduitB.racm).to.eql(['GET', 'PATCH', 'PUT']);
       });
+
+      let pickArecord, recordId, isEmailValid;
       it('EVS reads data using `conduit-B` and verifies if the email address is valid | by roll of a die', async function () {
-        const diceRoll = Math.floor(Math.random() * 6) + 1;
-        console.log('You rolled a ', diceRoll);
+        // get all records from spreadsheet
+        const res = await proxyServer().get('/').set('Host', conduitB.host);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.haveOwnProperty('records');
+        // console.log('>>> no of records:', res.body.records.length);
+        pickArecord = res.body.records;
+        pickArecord = util.pickRandomlyFrom(pickArecord);
+        // console.log('>>> You rolled dice and picked a record: ', pickArecord);
+        /* console.log(
+          '>>> email of the selected record:',
+          pickArecord.fields.email
+        ); */
+        isEmailValid = util.validateEmail(pickArecord.fields.email);
+        // console.log('>>> email vaild or invalid:', isEmailValid);
+        recordId = pickArecord.id;
       });
-      it('EVS marks a `row` to contain a valid email or not using `conduit-B`', async function () {});
+      it('EVS marks a `row` to contain a valid email or invalid email, using `conduit-B`', async function () {
+        const isValid = isEmailValid ? '1' : '0';
+
+        // should PATCH entries for email validation(partial update)
+        const req = {
+          records: [
+            {
+              id: recordId,
+              fields: {
+                isValid: isValid,
+              },
+            },
+          ],
+        };
+        const res = await proxyServer()
+          .patch('/')
+          .set('Host', conduitB.host)
+          .send(req);
+        // console.debug(res.body, res.status, res.error);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.haveOwnProperty('records');
+      });
     }
   );
   context(
@@ -198,7 +310,31 @@ describe('Registered user creates three conduits to a previously setup `contacts
       it('should have only read permission', async function () {
         expect(conduitC.racm).to.eql(['GET']);
       });
-      it('RAVS reads data using `conduit-C` and prepares a report ', async function () {});
+      it('RAVS reads data using `conduit-C` and prepares a report ', async function () {
+        // get all records from spreadsheet
+        const res = await proxyServer().get('/').set('Host', conduitC.host);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.haveOwnProperty('records');
+        for (let i = 0; i < res.body.records.length; i++) {
+          if (res.body.records[i].fields.isValid === '1') {
+            stats.processes.validCount += 1;
+          } else if (res.body.records[i].fields.isValid === '0') {
+            stats.processes.invalidCount += 1;
+          }
+        }
+        console.log(
+          '>>>> processed - valid count: ',
+          stats.processes.validCount,
+          ' invalid count: ',
+          stats.processes.invalidCount
+        );
+        console.log(
+          '>>> total records : ',
+          stats.submissions.realSubmissioncount,
+          ' bots count: ',
+          stats.submissions.botSubmissionCount
+        );
+      });
       it('The report contains: signups, valid-email, invalid-email | by day, by week, by month', async function () {});
     }
   );
